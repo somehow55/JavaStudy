72d9b78d529b239215fe9ca70097a47c470f00d4
somehow55 <79687989+somehow55@users.noreply.github.com>
2022-07-20 10:36:47 +0900

    220720 arrayList, HashMap / boolean, char, string
    Java 컬렉션, 자료형 공부

--- JavaStudy/src/main/java/study03/Pokemon.java
+++ JavaStudy/src/main/java/study03/Pokemon.java
@@ -2,9 +2,14 @@ package study03;
 
 public class Pokemon {
 
-	public Pokemon(String string) {
-//		int pokemonSize = string.length();
-		return;
+	private String name;
+
+	public Pokemon(String name) {
+		this.name = name;
 	}
 
+	@Override
+	public String toString() {
+		return name;
+	}
 }
---
+++ JavaStudy/src/main/java/study04_arrayList_HashMap/WordDictionary.java
@@ -0,0 +1,23 @@
+package study04_arrayList_HashMap;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class WordDictionary {
+	private HashMap<String, String> dictionaryBook = new HashMap<>();			// new HashMap 까먹지 말기 ㅋㅋ큐ㅠㅠㅠㅠ
+	
+	public void addWord(String key, String value) {
+		dictionaryBook.put(key, value);
+	}
+
+	public String find(String param) {
+//		return dictionaryBook.get(param);			 노놉.
+		
+		for (String key : dictionaryBook.keySet()) {
+			if(key.toLowerCase().equals(param.toLowerCase())) {
+				return dictionaryBook.get(key);
+			}
+		}
+		return param;
+	}
+}
